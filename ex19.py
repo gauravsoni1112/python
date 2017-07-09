c2ad1e5c586e2c25f02fe2f478597b6cc99eba7d
gauravsoni1112 <gsoni1112@gmail.com>
2017-07-09 18:34:02 +0200

    Functions and Variable

---
+++ ex19.py
@@ -0,0 +1,20 @@
+def cheese_and_crackers(cheese_count, boxes_of_crackers):
+      print "You have %d cheese!" % cheese_count
+      print "You have %d boxes of crackers!" % boxes_of_crackers
+      print "Man thats enough for the party"
+      print "Get a blanket.\n"
+
+print "We can just give the function number directly"
+
+cheese_and_crackers(20,30)
+
+print "Or we use a variable in the script"
+amount_of_cheese = 10
+amount_of_crackers = 50
+
+
+cheese_and_crackers(amount_of_cheese,amount_of_crackers)
+
+print "We can also do math operations !!"
+
+cheese_and_crackers(20+10,30+80)
